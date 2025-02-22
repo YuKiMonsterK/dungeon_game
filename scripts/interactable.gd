7970b3b2808c65f102d232e0614736ec22d9a082
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-02-22 18:17:55 +0800

    物品對話

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -3,18 +3,21 @@ extends Area2D
 
 signal interacted
 @export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
+var dialogue_title : String
+
 func _init():
 	collision_layer = 0
 	collision_mask = 0
-	set_collision_mask_value(2,true)
+	set_collision_mask_value(2, true)
 	
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
+func _ready():
+	dialogue_title = name
 func interact():
 	print("[Interacted] %s" % name)
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource,dialogue_start)
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_title)
 	interacted.emit()
 
 func _on_body_entered(player: Player):
